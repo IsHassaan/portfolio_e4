bdbe4e95afe4d3ee15bf24dd57def57e8b39f030
IsHassaan <is.hassaan27@gmail.com>
2022-05-23 09:37:20 +0200

    competences maj

--- src/js/competences.js
+++ src/js/competences.js
@@ -6,51 +6,52 @@ function Competences(){
     <div className="competences">         
         <div className="content">
             <img src={require('../assets/C_logo.png')} alt="competence" />
-            <p>There once lived an old man and an old woman who were peasants and had
-            to work hard to earn their daily bread. The old man used to go to fix
-            fences and do other odd jobs for the farmers around, and while he was
-            gone the old woman, his wife, did the work of the house and worked in
-            their own little plot of land</p>
+            <p>
+                <li>Création d'application console</li>
+                <li>Gestion de fonctions et procédures</li>
+                <li>développement en POO</li>
+                <li>Gestion de base de données SQL</li>
+            </p>
         </div>
 
         <div className="content">
             <img src={require('../assets/javascript.png')} alt="competence" />
-            <p>There once lived an old man and an old woman who were peasants and had
-            to work hard to earn their daily bread. The old man used to go to fix
-            fences and do other odd jobs for the farmers around, and while he was
-            gone the old woman, his wife, did the work of the house and worked in
-            their own little plot of land</p>
+            <p>
+                <li>Création éléments front-end</li>
+                <li>Gestion de fonctions et procédures</li>
+                <li>Liaison back-end (NodeJS)</li>
+                <li>Proprety binding</li>
+            </p>
         </div>
 
         <div className="content">
             <img src={require('../assets/Mysql_logo.png')} alt="competence" />
             <p>
-                La technologie MySQL lié à une application web ou à un logiciel
-                permet de gérer et stocker une lourde quantité de données de façon
-                dynamique.
-                Les données peuvent alors circuler en fonction des actions des 
-                utilisateurs et peuvent permettre de trier le contenue en fonction des 
-                besoins de ce dernier. 
+                <li>Création de base de données</li>
+                <li>Gestion de requêtes inter-table</li>
+                <li>Mise en place de paramètres de confidentialité</li>
+                <li>Liaison multi-plateformes</li>
             </p>
         </div>
 
         <div className="content">
             <img src={require('../assets/node_js.png')} alt="competence" />
-            <p>NodeJs permet la liaison des applications web aux différents serveurs mise 
-                en place par le développeur.
-                Ceci permet notamment de faire la liaison entre l'application et la base de 
-                donnée afin d'afficher ou de stocker des données de manière dynamique.
-                Cette technologie est au coeur de l'intéraction des applications web Reactjs.
+            <p>
+                <li>Liasion front-end</li>
+                <li>Liaison base de données SQL</li>
+                <li>Lecture de requêtes SQL</li>
+                <li>Méthode POST/GET des données SQL</li>
             </p>
         </div>
 
         <div className="content">
             <img src={require('../assets/python.png')} alt="competence" />
-            <p>There once lived an old man and an old woman who were peasants and had
-            to work hard to earn their daily bread. The old man used to go to fix
-            fences and do other odd jobs for the farmers around, and while he was
-            gone the old woman, his wife, did the work of the house and worked in
-            their own little plot of land</p>
+            <p>
+                <li>Création application console</li>
+                <li>Création projet mathématique</li>
+                <li>Gestion de fonctions et procédures</li>
+                <li>Import de bibliothèques externes</li>
+            </p>
         </div>
 
     </div>
